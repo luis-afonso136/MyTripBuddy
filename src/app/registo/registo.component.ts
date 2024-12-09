5103d089d64e2eb75033113a581c0ec0bfdfb10d
MariaJoseConceicao <maria.j.b.conceicao1311@gmail.com>
2024-12-09 15:00:15 +0000

    add_registo

---
+++ src/app/registo/registo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registo',
+  templateUrl: './registo.component.html',
+  styleUrls: ['./registo.component.scss'],
+})
+export class RegistoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
